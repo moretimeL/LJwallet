e6e04700f39cf6306c85463f55403e87de93dcb4
SmileL <1057720671@qq.com>
2020-10-20 17:55:52 +0800

    add background

--- app/src/main/java/com/liu/ljwallet/MainActivity.java
+++ app/src/main/java/com/liu/ljwallet/MainActivity.java
@@ -2,6 +2,7 @@ package com.liu.ljwallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBackground();
     }
 
+    public void initBackground(){
+        try {
+            Thread.sleep(1000);
+            Intent intent = new Intent(this, IndexActivity.class);
+            startActivity(intent);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
