0cdb02c415a7bb6dbc793cee900eefaecbd60c5e
liu <1057720671@qq.com>
2020-10-22 00:09:23 +0800

    add ExitApplication

--- app/src/main/java/com/liu/ljwallet/IndexActivity.java
+++ app/src/main/java/com/liu/ljwallet/IndexActivity.java
@@ -2,15 +2,20 @@ package com.liu.ljwallet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.liu.ljwallet.activity.CreateWalletActivity;
 import com.liu.ljwallet.activity.ImportWalletActivity;
+import com.liu.ljwallet.application.ExitApplication;
 
 public class IndexActivity extends AppCompatActivity {
+
+    private long exitTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,32 @@ public class IndexActivity extends AppCompatActivity {
         setContentView(R.layout.activity_index);
         initView();
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0) {
+                exitApp();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * 退出程序
+     */
+    private void exitApp() {
+        // 判断2次点击事件时间
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            // 退出程序关键代码
+            ExitApplication.getInstance().exit();
+        }
+    }
+
 
     public void initView(){
         Button crateButton = findViewById(R.id.create_wallet);
